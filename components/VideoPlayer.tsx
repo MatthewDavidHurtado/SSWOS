259bf30b578d9250d6103d3ecbee0eac8d2ed34d
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-08-23 05:54:08 -0700

    Fix Vimeo videos with referrer policy and bulletproof URL construction

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -5,6 +5,20 @@ interface VideoPlayerProps {
   video: Video | null;
 }
 
+function buildVimeoSrc(idWithHash: string) {
+  const [id, hash] = idWithHash.split('?h=');
+  const url = new URL(`https://player.vimeo.com/video/${id}`);
+  if (hash) url.searchParams.set('h', hash);
+  url.searchParams.set('autoplay', '1');
+  url.searchParams.set('badge', '0');
+  url.searchParams.set('autopause', '0');
+  url.searchParams.set('player_id', '0');
+  url.searchParams.set('app_id', '58479');
+  url.searchParams.set('dnt', '1'); // helps with some blockers
+  url.searchParams.set('muted', '1'); // helps with autoplay on mobile
+  return url.toString();
+}
+
 function VideoPlayer({ video }: VideoPlayerProps) {
   if (!video) {
     return (
@@ -18,18 +32,19 @@ function VideoPlayer({ video }: VideoPlayerProps) {
   if (video.platform === 'youtube') {
     videoSrc = `https://www.youtube.com/embed/${video.videoId}?autoplay=1&rel=0`;
   } else if (video.platform === 'vimeo') {
-    videoSrc = `https://player.vimeo.com/video/${video.videoId}&autoplay=1&badge=0&autopause=0&player_id=0&app_id=58479`;
+    videoSrc = buildVimeoSrc(video.videoId);
   }
 
   return (
     <div className="bg-black rounded-lg overflow-hidden">
-      <div className="relative w-full h-0 pb-[56.25%]"> {/* 16:9 Aspect Ratio */}
+      <div className="relative w-full h-0 pb-[56.25%]">
         <iframe
           className="absolute top-0 left-0 w-full h-full"
           src={videoSrc}
           title={video.title}
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+          referrerPolicy="origin"
           allowFullScreen
         ></iframe>
       </div>
