d6549f3c69117bc57f0cdce5ed0a0a1a214b16b3
MALCOLM KINGLEY <71328086+MatthewDavidHurtado@users.noreply.github.com>
2025-08-27 11:05:38 -0700

    Update What Is This Training button to link to sswos.com

--- components/PasswordProtection.tsx
+++ components/PasswordProtection.tsx
@@ -91,7 +91,7 @@ const PasswordProtection: React.FC<PasswordProtectionProps> = ({ onPasswordCorre
             </button>
 
             <a
-              href="https://the-secret-source-code-759385455270.us-west1.run.app/"
+              href="https://www.sswos.com"
               target="_blank"
               rel="noopener noreferrer"
               className="w-full bg-gray-700 text-white px-6 py-3 rounded-md font-bold text-lg hover:bg-gray-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-gray-500 text-center block"
